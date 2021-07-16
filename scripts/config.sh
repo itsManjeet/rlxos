d886f1909012b53661e0106bf9c56a39ec20c5cc
itsmanjeet <itsmanjeet1998@gmail.com>
2021-07-17 00:31:13 +0530

    Initialized

---
+++ scripts/config.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Generate PKGUPD configuration file
+
+set -u
+
+BASEDIR="$(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))"
+
+CARCH=${CARCH:-x86_64}
+RLXTGT=${CARCH}-rlxos-linux-gnu
+RLXTGT32=i686-rlxos-linux-gnu
+RLXOS=${BASEDIR}/build/rlxos.${RLXTGT}
+TOOLS=${RLXOS}/tools
+
+[[ ! -d ${TOOLS} ]] && mkdir -p ${TOOLS}
+
+echo "
+dir:
+    recipes: ${BASEDIR}/recipes
+    roots: ${RLXOS}
+    pkgs: ${BASEDIR}/pkgs
+    src: ${BASEDIR}/src
+    data: ${RLXOS}/var/lib/pkgupd/data
+    
+default:
+    repositories:
+        - toolchain
+
+environ:
+    - RLXTGT=${RLXTGT}
+    - RLXTGT32=${RLXTGT32}
+    - RLXOS=${RLXOS}
+    - TOOLS=${TOOLS}
+    - PATH=${TOOLS}/bin:/usr/bin:/opt/bin
+    - DESTDIR=\"\"
+" > ${BASEDIR}/pkgupd.yml
+
+if [[ ! -L /tools ]] || [[ $(realpath /tools) != ${TOOLS} ]] ; then
+    sudo ln -srv ${TOOLS} /tools
+fi

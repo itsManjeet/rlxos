e7180148c48114cab21fb8f250f806c65de34ff1
itsmanjeet <itsmanjeet1998@gmail.com>
2021-07-27 19:04:05 +0530

    fixed for actions

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -3,14 +3,6 @@
 BASEDIR="$(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))"
 composefile=${composefile:-${BASEDIR}/docker/docker-compose.yml}
 
-function startService() {
-    echo "starting service ${composefile}"
-    docker-compose -f ${composefile} up -d
-}
-
-container_id=$(docker-compose -f ${composefile} ps -q)
-[[ -z ${container_id} ]] && startService
-
 container_id=$(docker-compose -f ${composefile} ps -q)
 
 do_exec() {

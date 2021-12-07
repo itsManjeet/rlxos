8241ba2d9fba505a7c5b2d076d79d23b2f3a2d43
itsmanjeet <itsmanjeet1998@gmail.com>
2021-12-07 18:11:05 +0530

    sync

--- configure.py
+++ configure.py
@@ -4,11 +4,18 @@ import sys, os
 import yaml
 from jinja2 import Environment
 
+with open('.version') as f:
+    VERSION = f.read()
+
+
 for file in os.listdir('recipes/'):
     if file.endswith('.yml'):
         with open('recipes/{}'.format(file)) as f:
             data = f.read()
             obj = yaml.full_load(data)
-            data = Environment().from_string(data).render(obj)
-            with open('build/recipes/{}'.format(file),'w') as fw:
-                fw.write(data)
+            try:
+                data = Environment().from_string(data).render(obj)
+                with open('build/{}/recipes/{}'.format(VERSION,file),'w') as fw:
+                    fw.write(data)
+            except Exception as e:
+                print(str(e), file)

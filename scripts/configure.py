20024d4d48937e1e0fc7429872703b3954eacc02
Manjeet Singh <itsmanjeet1998@gmail.com>
2021-12-12 13:34:30 +0100

    Fixed configuration directory

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,21 +1,28 @@
-#!/bin/python
+#!/usr/bin/env python3
 
 import sys, os
 import yaml
+import shutil
 from jinja2 import Environment
 
 with open('.version') as f:
     VERSION = f.read()
 
+RECIPE_DIR = 'build/{}/recipes'.format(VERSION)
+if os.path.exists(RECIPE_DIR):
+    shutil.rmtree(RECIPE_DIR)
+
+os.makedirs(RECIPE_DIR)
 
 for file in os.listdir('recipes/'):
     if file.endswith('.yml'):
         with open('recipes/{}'.format(file)) as f:
             data = f.read()
             obj = yaml.full_load(data)
+            print('creating %s' % obj['id'])
             try:
                 data = Environment().from_string(data).render(obj)
-                with open('build/{}/recipes/{}'.format(VERSION,file),'w') as fw:
+                with open('{}/{}'.format(RECIPE_DIR,file),'w') as fw:
                     fw.write(data)
             except Exception as e:
                 print(str(e), file)

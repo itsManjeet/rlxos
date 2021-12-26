bff66fb00bce020ddf8f207afca237a4bd38de0b
itsmanjeet <itsmanjeet1998@gmail.com>
2021-12-26 19:41:01 +0530

    fixed basedir

--- scripts/compile-all.sh
+++ scripts/compile-all.sh
@@ -3,7 +3,7 @@
 BASEDIR="$(
     cd -- "$(dirname "$0")" >/dev/null 2>&1
     pwd -P
-)"
+)/../"
 
 VERSION=$(cat ${BASEDIR}/.version)
 
@@ -12,7 +12,7 @@ PKGS=$(ls ${BASEDIR}/build/${VERSION}/recipes/ | sed 's|.yml||g')
 for pkg in ${PKGS}; do
     echo "compiling for ${pkg}"
     LOGFILE="${BASEDIR}/build/${VERSION}/logs/${pkg}.log"
-    ${BASEDIR}/compile.sh ${pkg} | tee ${LOGFILE}
+    ${BASEDIR}/scripts/compile.sh ${pkg} | tee ${LOGFILE}
     if [[ ${?} != 0 ]]; then
         echo "Failed to compile ${pkg}"
         mv "${LOGFILE}" "${LOGFILE}.failed"

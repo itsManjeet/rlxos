ad5e0067164dd525043fdbe906a60b0f0899f575
rlxos-bolt <rlxos.bolt@gmail.com>
2022-01-05 09:21:27 +0100

    [Fixed]: sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -18,22 +18,31 @@ for i in ${PKGDIR}/*.rlx; do
         echo "Error! failed to register ${i}"
         continue
     fi
+    echo "    packages:" >> ${PKGDIR}/recipe
+    echo "      - id: pkg" >> ${PKGDIR}/recipe
+    echo "        pack: rlx" >> ${PKGDIR}/recipe
+    echo "        dir: ." >> ${PKGDIR}/recipe
 done
 
 for i in ${PKGDIR}/*.app; do
-    rm -rf squashfs-root
     ${i} --appimage-extract info
     head_id="  - $(cat squashfs-root/info | head -n1)"
     echo "${head_id}" >> ${PKGDIR}/recipe
     echo "    type: app" >> ${PKGDIR}/recipe
     tail -n+2 squashfs-root/info | sed 's/^/    /' >>${PKGDIR}/recipe
+    echo "    packages:" >> $PKGDIR/recipe
+    echo "      - id: pkg" >> ${PKGDIR}/recipe
+    echo "        pack: app" >> ${PKGDIR}/recipe
+    echo "        dir: ." >> ${PKGDIR}/recipe
+
+    rm -rf squashfs-root
 done
 source ./secure/storage
 
-# lftp -e "
-# set ftp:ssl-allow no
-# open ${STORAGE_URL}
-# user ${STORAGE_USERNAME} ${STORAGE_PASSWORD}
-# mirror --reverse --verbose ${BASEDIR}/build/ ${STORAGE_PATH}/
-# bye
-# "
+lftp -e "
+set ftp:ssl-allow no
+open ${STORAGE_URL}
+user ${STORAGE_USERNAME} ${STORAGE_PASSWORD}
+mirror --reverse --verbose ${BASEDIR}/build/ ${STORAGE_PATH}/
+bye
+"

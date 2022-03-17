145ec52df3c665823406fe39e67a76f72b87b7a4
itsmanjeet <itsmanjeet1998@gmail.com>
2022-03-17 17:25:49 +0100

    fixed script for PKGUPD multi repository support

--- scripts/configure.py
+++ scripts/configure.py
@@ -12,22 +12,27 @@ RECIPE_DIR = 'build/{}/recipes'.format(VERSION)
 if os.path.exists(RECIPE_DIR):
     shutil.rmtree(RECIPE_DIR)
 
-os.makedirs(RECIPE_DIR)
-
 FAILED = []
 
-for file in os.listdir('recipes/'):
-    if file.endswith('.yml'):
-        with open('recipes/{}'.format(file)) as f:
-            data = f.read()
-            obj = yaml.full_load(data)
+for repository in os.listdir('recipes/'):
+    print("=> configuring {}".format(repository))
+    os.makedirs(RECIPE_DIR + '/' + repository)
+    for file in os.listdir('recipes/{}/'.format(repository)):
+        if file.endswith('.yml'):
             try:
-                if 'packages' in obj and len(obj['packages']) != 1:
-                    FAILED.append(file)
-                data = Environment().from_string(data).render(obj)
-                with open('{}/{}'.format(RECIPE_DIR,file),'w') as fw:
-                    fw.write(data)
+                with open('recipes/{}/{}'.format(repository, file)) as f:
+                    data = f.read()
+                    obj = yaml.full_load(data)
+                    try:
+                        if 'packages' in obj and len(obj['packages']) != 1:
+                            FAILED.append(file)
+                        data = Environment().from_string(data).render(obj)
+                        with open('{}/{}/{}'.format(RECIPE_DIR, repository, file),'w') as fw:
+                            fw.write(data)
+                    except Exception as e:
+                        FAILED.append(file)
             except Exception as e:
+                print("failed in {} {}".format(file, e))
                 FAILED.append(file)
 
 print("failed", FAILED)

6da746708b4026b523fae4343c72188e566b989f
itsmanjeet <itsmanjeet1998@gmail.com>
2022-03-25 19:03:54 +0100

    [FIXED]: packages for build

--- scripts/compile-all.sh
+++ scripts/compile-all.sh
@@ -15,14 +15,18 @@ DEPENDS_FILE="${BASEDIR}/build/depends"
 if [[ -f ${DEPENDS_FILE} ]] ; then
     DEPENDS=$(cat ${DEPENDS_FILE})
 else
-    DEPENDS=$(${BASEDIR}/scripts/exec.sh pkgupd depends ${PKGS} --force)
+    DEPENDS=$(${BASEDIR}/scripts/exec.sh pkgupd depends ${PKGS} --force 2>&1)
+    if [[ $? != 0 ]] ; then
+        echo "Failed to calculate dependencies ${DEPENDS}"
+        exit 1
+    fi
     echo "${DEPENDS}" > ${DEPENDS_FILE}
 fi
 
 for pkg in ${DEPENDS}; do
     # BoltSendMesg "compiling for ${pkg}"
     LOGFILE="${BASEDIR}/build/${VERSION}/logs/${pkg}.log"
-    ${BASEDIR}/scripts/exec.sh pkgupd co ${pkg} --force 2>&1 | sed -r 's/\x1b\[[0-9;]*m//g' | tee ${LOGFILE}
+    ${BASEDIR}/scripts/exec.sh pkgupd co ${pkg} 2>&1 | sed -r 's/\x1b\[[0-9;]*m//g' | tee ${LOGFILE}
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
         # BoltSendMesg "Failed to compile ${pkg}"
         mv "${LOGFILE}" "${LOGFILE}.failed"

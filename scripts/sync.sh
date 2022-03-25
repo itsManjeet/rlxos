7fd6c6670523d0b47d7a7c132e16c5c5c1c18ce4
itsmanjeet <itsmanjeet1998@gmail.com>
2022-03-25 16:49:57 +0100

    Fixed for build

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 
-BASEDIR="$(
-    cd -- "$(dirname "$0")" >/dev/null 2>&1
-    pwd -P
-)/../"
+VERSION=${VERSION:-$(cat .version)}
 
-VERSION=$(cat ${BASEDIR}/.version)
-PKGDIR="${BASEDIR}/build/${VERSION}/pkgs"
-RECIPEDIR="${BASEDIR}/build/${VERSION}/recipes"
-
-${BASEDIR}/scripts/configure.py
-echo "version: ${VERSION}
-recipes:" >${PKGDIR}/recipe
-for i in ${RECIPEDIR}/*.yml; do
-    head -n1 ${i} | sed 's/^/  - /' >>${PKGDIR}/recipe
-    tail -n+2 ${i} | sed 's/^/    /' >>${PKGDIR}/recipe
-    if [[ ${?} -ne 0 ]]; then
-        echo "Error! failed to register ${i}"
-        continue
-    fi
-    echo "" >> ${PKGDIR}/recipe
+for repo in build/${VERSION}/pkgs/* ; do
+    [[ ! -d ${repo} ]] && continue
+    echo "syncing $(basename ${repo})"
+    pkgupd gen-sync ${VERSION} ${repo}
 done
-source ./secure/storage
-
-rm -rf ${RECIPEDIR}
-
-lftp -e "
-set ftp:ssl-allow no
-open ${STORAGE_URL}
-user ${STORAGE_USERNAME} ${STORAGE_PASSWORD}
-mirror --reverse --verbose ${BASEDIR}/build/ ${STORAGE_PATH}/
-bye
-"

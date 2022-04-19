45d9097286a4232fc934d788ffc3ae10e3284690
itsmanjeet <itsmanjeet1998@gmail.com>
2022-04-19 18:05:52 +0530

    :: fixed cleanup .la scripts

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -12,8 +12,8 @@ export PKGUPD_NO_PROGRESS=1
 echo ":: updating pkgupd"
 pkgupd in pkgupd --force --no-depends
 
-# echo ":: executing patch ::"
-# find /lib/ -name "*.la" -delete -print
+echo ":: executing patch ::"
+find /lib/ -name "*.la" -delete
 
 echo "::updating system ::"
 pkgupd update --no-ask

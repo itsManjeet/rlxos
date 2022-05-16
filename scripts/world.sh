d80b5206e999661492c509d5d4195271697dc018
itsmanjeet <itsmanjeet1998@gmail.com>
2022-05-16 12:32:54 +0530

    fixed world script

---
+++ scripts/world.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASEDIR="$(
+    cd -- "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)/../"
+
+function RunInContainer() {
+    ${BASEDIR}/scripts/exec.sh ${@}
+}
+
+ALL_PKGS=$(find ${BASEDIR}/build/${VERSION}/recipes/ -type f -name "*.yml" -exec basename {} \; | sed 's|.yml||g')
+
+echo ":: generating dependency tree ::"
+DEPENDENCY_TREE=$(RunInContainer pkgupd depends --force ${ALL_PKGS})
+if [[ $? != 0 ]] ; then
+    echo "failed to build dependency tree ${DEPENDENCY_TREE}"
+    exit 1
+fi
+
+for pkg in ${DEPENDENCY_TREE} ; do
+    pkg=$(echo ${pkg} | tr -cd '[:print:]')
+    echo ":: compiling ${pkg} ::"
+    RunInContainer pkgupd co ${pkg}
+done

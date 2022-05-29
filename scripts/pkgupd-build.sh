debce1f95467502e92532c86c572dc769f63128b
Manjeet Singh <itsmanjeet@vmi819861.contaboserver.net>
2022-05-29 18:40:09 +0200

    updated buildscript

--- scripts/pkgupd-build.sh
+++ scripts/pkgupd-build.sh
@@ -12,7 +12,9 @@ echo "updating system"
 pkgupd update mode.ask=false
 
 for i in ${@} ; do
-    DEBUG=1 pkgupd build build.recipe=/var/cache/pkgupd/${i} package.repository=$(echo ${i} | cut -d '/' -f2) mode.ask=false
+    DEBUG=1 pkgupd build /var/cache/pkgupd/${i} \
+        build.repository=$(echo ${i} | cut -d '/' -f2) \
+        mode.ask=false
     if [[ $? != 0 ]] ; then
         echo "Error! failed to build ${i}"
         exit 1

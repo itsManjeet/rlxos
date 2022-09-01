8d4b0b0b762f420e86a2bbfa74fe1956444337db
itsmanjeet <itsmanjeet1998@gmail.com>
2022-09-01 16:03:56 +0200

    scripts: fixed automation scripts

--- scripts/inside-docker.sh
+++ scripts/inside-docker.sh
@@ -5,6 +5,8 @@ docker run  \
     -v ${PWD}:/rlxos \
     -v ${PWD}/pkgupd.testing.yml:/etc/pkgupd.yml \
     -v /storage/testing/2200/pkgs:/var/cache/pkgupd/pkgs/ \
+    -v ${PWD}/scripts:/scripts \
+    -v ${PWD}/recipes:/var/cache/pkgupd/recipes \
     -w /rlxos \
     -it itsmanjeet/rlxos-devel:2200-1 \
     bash

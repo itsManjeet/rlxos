8f8dc49d3fc97f747a97200676f78abe8d47527b
itsmanjeet <itsmanjeet1998@gmail.com>
2022-09-01 16:03:56 +0200

    scripts: fixed automation scripts

--- scripts/update-bazaar.py
+++ scripts/update-bazaar.py
@@ -2,6 +2,7 @@
 
 import yaml
 import os
+import sys
 from woocommerce import API
 
 
@@ -19,20 +20,67 @@ wcapi = API(
 )
 
 all_products_resp = wcapi.get('products')
+print("status: %d" % all_products_resp.status_code)
+
+if all_products_resp.status_code != 200 and \
+        all_products_resp.status_code != 201:
+    print("Failed to retrieve code %d, %s" %
+          (all_products_resp.status_code, all_products_resp.json()['message']))
+    sys.exit(1)
+
+bazaar_cache = all_products_resp.json()
 
 def getPackage(name: str) -> int:
-    for product in all_products_resp.json():
+    for product in bazaar_cache:
         if product['name'] == name:
             return product['id']
     return -1
 
+for product in bazaar_cache:
+    print('BAZAAR %s' % product['name'])
+
+bulk_cache = {
+    'create': [],
+    'update': [],
+    'delete': []
+}
+
+
+bazaar_idx = -1
+for package in bazaar_cache:
+    bazaar_idx += 1
+    local_package_data = {}
+    idx = -1
+    for i in repositories:
+        idx += 1
+        if i['id'] == package['name']:
+            local_package_data = i
+            break
+
+    if len(local_package_data) == 0:
+        print('deleting %s' % package['name'])
+        bulk_cache['delete'].append(package['id'])
+    else:
+        print('updating %s' % package['name'])
+        bulk_cache['update'].append({
+            'id': package['id'],
+            'downloads': [{
+                'name': 'Package File',
+                'file': 'https://storage.rlxos.dev/stable/2200/pkgs/%s/%s-%s.%s' % (i['repository'], i['id'], i['version'], i['type'])
+            }]
+        })
+    repositories.pop(idx)
+    bazaar_cache.pop(bazaar_idx)
+
+for product in bazaar_cache:
+    bulk_cache['delete'].append(product['id'])
 
 for package in repositories:
-    id = getPackage(package['id'])
+    print('adding %s' % package['id'])
     category_id = 35
     if package['repository'] == 'apps':
         category_id = 46
-    data = {
+    bulk_cache['create'].append({
         'name': package['id'],
         'type': 'simple',
         'description': package['about'],
@@ -48,14 +96,5 @@ for package in repositories:
         'categories': [{
             'id': category_id,
         }]
-    }
-    if id == -1:
-        print("adding package", package['id'])
-        resp = wcapi.post('products', data=data)
-    else:
-        data['id'] = id
-        print("updating product %d" % id)
-        resp = wcapi.post('products/%d' % id, data=data)
-    
-    if resp.status_code != 200 and resp.status_code != 201:
-        print('response', resp.status_code, resp.text)
+    })
+

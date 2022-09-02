db04d8f1c4214e374bea11df3233cf13584c3aa9
itsmanjeet <itsmanjeet1998@gmail.com>
2022-09-02 10:09:01 +0200

    scripts: fixed bazaar icon push

--- scripts/update-bazaar.py
+++ scripts/update-bazaar.py
@@ -20,12 +20,18 @@ if package['repository'] == 'core' or \
     package['repository'] == 'extra':
     sys.exit(0)
 
+icon_file = ''
+for ext in ['png', 'svg', 'jpg', 'jpeg']:
+    if os.path.exists(meta_file.replace('.yml', '.' + ext)):
+        icon_file = meta_file.replace('.yml', '.' + ext)
+        break
+
 response = wcapi.get('products?slug=%s' % package['id'])
 category_id = 35
 if package['repository'] == 'apps':
     category_id = 46
 if len(response.json()) == 0:
-    wcapi.post('products', data = {
+    meta_data = {
         'name': package['id'],
         'type': 'simple',
         'description': package['about'],
@@ -35,18 +41,26 @@ if len(response.json()) == 0:
             'name': 'Package File',
             'file': 'https://storage.rlxos.dev/stable/2200/pkgs/%s/%s-%s.%s' % (package['repository'], package['id'], package['version'], package['type'])
         }],
-        'images': [{
-            'src': 'https://storage.rlxos.dev/icons/%s.svg' % package['id'],
-        }],
         'categories': [{
             'id': category_id,
         }]
-    })
+    }
+    if len(icon_file) != 0:
+        meta_data['images'] = [{
+            'src': 'https://storage.rlxos.dev/' + icon_file,
+        }],
+    response = wcapi.post('products', data = meta_data)
 else:
-    wcapi.post('products/%d' % response.json()[0]['id'], data = {
+    response = wcapi.post('products/%d' % response.json()[0]['id'], data = {
         'description': package['about'],
         'downloads': [{
             'name': 'Package File',
             'file': 'https://storage.rlxos.dev/stable/2200/pkgs/%s/%s-%s.%s' % (package['repository'], package['id'], package['version'], package['type'])
         }]
     })
+
+if response.status_code == 200 or \
+    response.status_code == 201:
+    print('success')
+else:
+    print(response.status_code, response.json()['message'])

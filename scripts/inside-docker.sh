4695077a87724d42cdf9ebe6cbcb2336f24ebe7a
itsmanjeet <itsmanjeet1998@gmail.com>
2022-09-14 08:32:32 +0200

    sync

--- scripts/inside-docker.sh
+++ scripts/inside-docker.sh
@@ -5,6 +5,7 @@ docker run                                                          \
     --privileged                                                    \
     -v ${PWD}:/rlxos                                                \
     -v ${PWD}/pkgupd.yml:/etc/pkgupd.yml                            \
+    -v /var/run/docker.sock:/var/run/docker.sock                    \
     -v ${STORAGE_DIR}:/storage                                      \
     -w /rlxos                                                       \
     -it itsmanjeet/rlxos-devel:2200-2                               \

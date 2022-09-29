8b66c4c3168764ea0514b53a6407d5829698db54
itsmanjeet <itsmanjeet1998@gmail.com>
2022-09-29 15:36:46 +0200

    grub: updated for newer system

--- recipes/core/grub/update-grub.sh
+++ recipes/core/grub/update-grub.sh
@@ -5,6 +5,6 @@ if [[ "$EUID" -ne 0 ]] ; then
     exit
 fi
 
-GRUB_DEVICE=$(mount | grep " /run/initramfs " | awk '{print $1}')    \
-GRUB_DEVICE_BOOT=${GRUB_DEVICE}                                      \
-grub-mkconfig -o /run/initramfs/boot/grub/grub.cfg
+GRUB_DEVICE=$(mount | grep " /boot " | awk '{print $1}')    \
+GRUB_DEVICE_BOOT=${GRUB_DEVICE}                             \
+grub-mkconfig -o /boot/grub/grub.cfg

999011278ab316bc24912637c6dcb45fa35c7452
itsmanjeet <itsmanjeet1998@gmail.com>
2023-08-08 13:30:40 +0200

    components/core: fixed installation issues

--- src/installer/backend/backend.go
+++ src/installer/backend/backend.go
@@ -2,22 +2,30 @@ package backend
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path"
 	"rlxos/pkg/utils"
 	swupd "rlxos/src/swupd/backend"
+	"strings"
 	"syscall"
+	"unicode"
 )
+
+type ProgressFunction func(int, string)
 
 type Backend struct {
 	ParititonType string
 	ImageVersion  int
 	KernelVersion string
+	Timeout       int
+	PrettyName    string
+	Progress      ProgressFunction
 }
 
-func New() (*Backend, error) {
+func New(progress ProgressFunction) (*Backend, error) {
 	imageVersion, err := swupd.GetCurrentVersion()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get current version %v", err)
@@ -30,7 +38,14 @@ func New() (*Backend, error) {
 		ParititonType: "ext4",
 		ImageVersion:  imageVersion,
 		KernelVersion: string(kernelVersion),
+		Progress:      progress,
+		PrettyName:    "RLXOS Linux",
+		Timeout:       10,
 	}, nil
+}
+
+func (b *Backend) partitionToDisk(part string) string {
+	return strings.TrimRightFunc(part, unicode.IsDigit)
 }
 
 func (b *Backend) Install(part string) error {
@@ -66,7 +81,7 @@ func (b *Backend) Install(part string) error {
 	if err := os.MkdirAll(path.Join(sysroot, "boot", "grub"), 0755); err != nil {
 		return fmt.Errorf("failed to create boot directories, %v", err)
 	}
-	if data, err := exec.Command("grub-install", "--recheck", "--root-directory="+sysroot, "--boot-directory="+path.Join(sysroot, "boot"), part).CombinedOutput(); err != nil {
+	if data, err := exec.Command("grub-install", "--recheck", "--root-directory="+sysroot, "--boot-directory="+path.Join(sysroot, "boot"), b.partitionToDisk(part)).CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to install bootloader %s, %v", string(data), err)
 	}
 
@@ -78,6 +93,31 @@ func (b *Backend) Install(part string) error {
 	log.Println("Generating initramfs")
 	if data, err := exec.Command("mkinitramfs", "-o="+path.Join(sysroot, "boot", "initramfs-"+b.KernelVersion+".img"), "--no-plymouth").CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to generate initramfs %s, %v", string(data), err)
+	}
+
+	log.Println("Getting UUID for", part)
+	grubRootPath := part
+	if data, err := exec.Command("sh", "-c", fmt.Sprintf("lsblk -o path,uuid  | grep %s | awk '{print $2}'", part)).CombinedOutput(); err == nil {
+		grubRootPath = strings.Trim(string(data), " \n")
+		if len(grubRootPath) == 0 {
+			grubRootPath = part
+		} else {
+			grubRootPath = "UUID=" + grubRootPath
+		}
+	}
+
+	log.Println("Writing bootloader configuration")
+	if err := ioutil.WriteFile(path.Join(sysroot, "boot", "grub", "grub.cfg"), []byte(fmt.Sprintf(`
+set timeout=%d
+set default="%s Installer"
+
+menuentry "%s Installer" {
+	linux /boot/vmlinuz-%s rw system=%d root=UUID=%s
+	initrd /boot/initramfs-%s.img
+}
+
+	`, b.Timeout, b.PrettyName, b.PrettyName, b.KernelVersion, b.ImageVersion, grubRootPath, b.KernelVersion)), 0644); err != nil {
+		return fmt.Errorf("failed to write bootloader configuration %v", err)
 	}
 
 	return nil

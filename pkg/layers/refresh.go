6c5d475cfcf010c0a55e459c0a6de418f03000a6
itsmanjeet <itsmanjeet1998@gmail.com>
2023-08-31 18:20:07 +0530

    fixed depmod integrations failure issue

--- pkg/layers/refresh.go
+++ pkg/layers/refresh.go
@@ -1,12 +1,20 @@
 package layers
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"path"
+	"strings"
 	"syscall"
 )
 
 func (m *Manager) Refresh() error {
+	if len(m.Layers) == 0 {
+		log.Println("no layer found, skipping")
+		return nil
+	}
+
 	var flag uintptr = 0
 	isMounted, err := checkIsMounted()
 	if err != nil {
@@ -15,13 +23,36 @@ func (m *Manager) Refresh() error {
 	if isMounted {
 		flag = syscall.MS_REMOUNT
 	}
-	lower := m.RootDir
+
+	lower := m.RootDir + "/usr"
+	withReadWrite := ""
 	for _, l := range m.Layers {
+		if strings.HasPrefix(l.Id, ".") || strings.HasPrefix(path.Base(l.Path), ".") {
+			continue
+		}
+
+		if l.Id == "rw" || path.Base(l.Path) == "rw" {
+			withReadWrite = path.Join(m.RootDir, l.Path)
+			continue
+		}
+
+		if l.Id == "work" || path.Base(l.Path) == "work" {
+			continue
+		}
+
 		log.Printf("enabling layer %s\n", l.Id)
 		lower += ":" + path.Join(m.RootDir, l.Path)
 	}
-	log.Println("LOWERDIR", lower)
-	if err := syscall.Mount("overlay", m.RootDir, "overlay", flag, "lowerdir="+lower); err != nil {
+	options := "lowerdir=" + lower
+	if len(withReadWrite) != 0 {
+		workdir := path.Join(path.Dir(withReadWrite), "work")
+		if err := os.MkdirAll(workdir, 0755); err != nil {
+			log.Printf("failed to create workdir %s, %v", workdir, err)
+		}
+		options = fmt.Sprintf("%s,upperdir=%s,workdir=%s", options, withReadWrite, workdir)
+	}
+	log.Println("OPTIONS", options)
+	if err := syscall.Mount("overlay", m.RootDir+"/usr", "overlay", flag, options); err != nil {
 		return err
 	}
 	return nil

7d03f00f8f30945b2d3195183b6f7d9134573134
itsmanjeet <itsmanjeet1998@gmail.com>
2024-03-08 12:53:22 +0530

    cleanup

--- src/srclang/Language/Options.cxx
+++
@@ -1,7 +0,0 @@
-#include "Options.hxx"
-
-using namespace srclang;
-
-Options::Options(std::map<std::string, OptionType> const &options)
-    : std::map<std::string, OptionType>(options) {
-}
--- src/srclang/Value/Function.cxx
+++
@@ -1,4 +0,0 @@
-#include "Function.hxx"
-
-namespace srclang {
-}  // srclang

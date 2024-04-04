d58ceeea7449c6b16bfbde16ae9e8bf36d99fff8
itsmanjeet <itsmanjeet1998@gmail.com>
2024-04-04 13:17:04 +0200

    fixed cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,12 +2,12 @@
 
 # TODO: better way to cleanup these
 
-if [[ -z ${CACHE_PATH} ]] ; then
+if [ -z "${CACHE_PATH}" ] ; then
     echo "No cache path povided"
     exit 1
 fi
 
-rm -f ${CACHE_PATH}/cache/system-repo-*.pkg*
-rm -f ${CACHE_PATH}/cache/system-deps-*.pkg*
-rm -f ${CACHE_PATH}/cache/installer-image-*.pkg*
-rm -f ${CACHE_PATH}/cache/apps-*.pkg*
+rm -f "${CACHE_PATH}"/cache/system-repo-*.pkg*
+rm -f "${CACHE_PATH}"/cache/system-deps-*.pkg*
+rm -f "${CACHE_PATH}"/cache/installer-image-*.pkg*
+rm -f "${CACHE_PATH}"/cache/apps-*.pkg*

c1e4a95dae629cdc5915186be76f77b82b267e0f
itsmanjeet <itsmanjeet1998@gmail.com>
2024-04-04 13:12:47 +0200

    components/interface: replaced with desktop

--- dist/js/SearchProvider.js
+++
@@ -1,8 +0,0 @@
-class SearchProvider {
-    constructor() {
-    }
-
-    search(text, notificationManager) {
-        notificationManager.notify("dev.rlxos.SearchProvider", `No result found for '${text}'`);
-    }
-}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/itsmanjeet/rlxos
-
-go 1.18

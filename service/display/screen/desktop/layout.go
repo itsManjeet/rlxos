35cf41d977f310d51e3d672f9065893a91c867f2
Manjeet Singh <33650320+itsManjeet@users.noreply.github.com>
2025-07-10 21:10:48 +0530

    Display server (#315)
    * service/display: added spacing in layout with rounded corner surfaces
    
    * README.md: updated screenshot

--- service/display/screen/desktop/layout.go
+++ service/display/screen/desktop/layout.go
@@ -21,43 +21,54 @@ import "image"
 
 func (d *Desktop) Layout() {
 	count := len(d.surfaces)
+
 	if count == 0 {
 		d.SetDirty(true)
 		return
 	}
+
 	d.activeSurface = d.surfaces[len(d.surfaces)-1]
 
 	bounds := d.Bounds()
 	screenWidth, screenHeight := bounds.Dx(), bounds.Dy()
+
 	masterRatio := 0.6
+	padding := 10
 
 	if count == 1 {
-		d.surfaces[0].SetBounds(bounds)
+		d.surfaces[0].SetBounds(image.Rect(
+			bounds.Min.X+padding,
+			bounds.Min.Y+padding,
+			bounds.Max.X-padding,
+			bounds.Max.Y-padding,
+		))
 		d.SetDirty(true)
 		return
 	}
 
-	masterWidth := int(float64(screenWidth) * masterRatio)
+	masterWidth := int(float64(screenWidth)*masterRatio) - padding
 	stackCount := count - 1
-	stackHeight := screenHeight / stackCount
+	stackHeight := (screenHeight - padding*(stackCount+1)) / stackCount
 
 	d.surfaces[0].SetBounds(image.Rect(
-		bounds.Min.X,
-		bounds.Min.Y,
-		bounds.Min.X+masterWidth,
-		bounds.Max.Y,
+		bounds.Min.X+padding,
+		bounds.Min.Y+padding,
+		bounds.Min.X+padding+masterWidth,
+		bounds.Max.Y-padding,
 	))
 
 	for i := 0; i < stackCount; i++ {
-		top := bounds.Min.Y + i*stackHeight
+		top := bounds.Min.Y + padding + i*(stackHeight+padding)
 		bottom := top + stackHeight
+
 		if i == stackCount-1 {
-			bottom = bounds.Max.Y
+			bottom = bounds.Max.Y - padding
 		}
+
 		d.surfaces[i+1].SetBounds(image.Rect(
-			bounds.Min.X+masterWidth,
+			bounds.Min.X+padding+masterWidth+padding,
 			top,
-			bounds.Max.X,
+			bounds.Max.X-padding,
 			bottom,
 		))
 	}

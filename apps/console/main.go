2a1e5ed155e67f08297440a41560dcfb91c736e1
itsmanjeet <itsmanjeet1998@gmail.com>
2025-08-02 16:43:10 +0000

    Use cell system for console view

--- apps/console/main.go
+++ apps/console/main.go
@@ -21,10 +21,16 @@ import (
 	"image"
 	"log"
 	"os/exec"
+	"slices"
+	"strconv"
+	"strings"
 
 	"rlxos.dev/pkg/event"
 	"rlxos.dev/pkg/event/key"
+	"rlxos.dev/pkg/graphics"
 	"rlxos.dev/pkg/graphics/app"
+	"rlxos.dev/pkg/graphics/argb"
+	"rlxos.dev/pkg/graphics/canvas"
 	"rlxos.dev/pkg/graphics/widget"
 	"rlxos.dev/pkg/kernel/vt"
 )
@@ -33,8 +39,12 @@ type Console struct {
 	widget.Base
 
 	fontSize   int
+	cellSize   image.Point
 	vt         *vt.VT
 	rows, cols int
+	cursor     image.Point
+	buffer     [][]byte
+	back       []byte
 }
 
 func (c *Console) Construct() {
@@ -53,6 +63,10 @@ func (c *Console) Construct() {
 	})
 
 	c.fontSize = 8
+	c.cellSize = image.Point{
+		X: 8,
+		Y: 12,
+	}
 }
 
 func (c *Console) SetBounds(rect image.Rectangle) {
@@ -62,17 +76,99 @@ func (c *Console) SetBounds(rect image.Rectangle) {
 	}
 	c.Base.SetBounds(rect)
 
-	chWidth, chHeight := 8, 8
-	c.cols = rect.Dx() / chWidth
-	c.rows = rect.Dy() / chHeight
+	c.cols = rect.Dx() / c.cellSize.X
+	c.rows = rect.Dy() / c.cellSize.Y
 	log.Println("Setting size:", c.cols, c.rows)
 	_ = c.vt.Resize(uint16(c.cols), uint16(c.rows))
+
+	c.buffer = make([][]byte, c.rows)
+	for i := 0; i < c.rows; i++ {
+		c.buffer[i] = make([]byte, c.cols)
+	}
+}
+
+func (c *Console) Draw(cv canvas.Canvas) {
+	graphics.FillRect(cv, c.Bounds(), 0, argb.NewColor(0, 0, 0, 255))
+	for y, row := range c.buffer {
+		for x, ch := range row {
+			pos := image.Point{
+				X: x*c.cellSize.X + c.cellSize.X,
+				Y: y*c.cellSize.Y + c.cellSize.Y,
+			}
+			if x == c.cursor.X && y == c.cursor.Y {
+				graphics.Text(cv, pos, string(ch), c.fontSize, argb.NewColor(0, 0, 0, 255))
+			} else {
+				graphics.Text(cv, pos, string(ch), c.fontSize, argb.NewColor(255, 255, 255, 255))
+			}
+		}
+	}
+	c.Base.Draw(cv)
 }
 
 func (c *Console) Update(ev event.Event) bool {
 	switch ev := ev.(type) {
 	case VTEvent:
+		if c.back != nil {
+			ev = append(c.back, ev...)
+			c.back = nil
+		}
+
 		log.Println("Got pty event:", string(ev))
+		for i := 0; i < len(ev); i++ {
+			ch := ev[i]
+
+			switch ch {
+			case 0x1b:
+				if len(ev[i:]) < 2 || ev[i+1] != '[' {
+					continue
+				}
+				idx := slices.IndexFunc(ev[i+2:], func(r byte) bool {
+					return r >= '@' && r <= '~'
+				})
+
+				if idx == -1 {
+					c.back = ev[i:]
+					return true
+				}
+
+				c.processEscape(ev[i+2 : i+2+idx])
+				i += 2 + idx
+
+			case '\n':
+				c.cursor.X = 0
+				c.cursor.Y++
+				if c.cursor.Y >= c.rows {
+					c.scrollUp()
+					c.cursor.Y = c.rows - 1
+				}
+			case '\r':
+				c.cursor.X = 0
+			case '\b':
+				if c.cursor.X > 0 {
+					c.cursor.X--
+					c.buffer[c.cursor.Y][c.cursor.X] = ' '
+				} else if c.cursor.Y > 0 {
+					c.cursor.Y--
+					c.cursor.X = c.cols - 1
+					c.buffer[c.cursor.Y][c.cursor.X] = ' '
+				}
+			default:
+				if ch >= 32 && ch < 127 {
+					if c.cursor.Y < len(c.buffer) && c.cursor.X < len(c.buffer[c.cursor.Y]) {
+						c.buffer[c.cursor.Y][c.cursor.X] = ch
+					}
+					c.cursor.X++
+					if c.cursor.X >= c.cols {
+						c.cursor.X = 0
+						c.cursor.Y++
+						if c.cursor.Y >= c.rows {
+							c.scrollUp()
+							c.cursor.Y = c.rows - 1
+						}
+					}
+				}
+			}
+		}
 		c.SetDirty(true)
 
 	case key.Event:
@@ -96,6 +192,61 @@ func (c *Console) Update(ev event.Event) bool {
 		}
 	}
 	return true
+}
+
+func (c *Console) scrollUp() {
+	copy(c.buffer[0:], c.buffer[1:])
+	c.buffer[c.rows-1] = make([]byte, c.cols)
+	for x := range c.buffer[c.rows-1] {
+		c.buffer[c.rows-1][x] = ' '
+	}
+}
+
+func (c *Console) processEscape(buf []byte) {
+	if len(buf) == 0 {
+		return
+	}
+	final := buf[len(buf)-1]
+	params := string(buf[:len(buf)-1])
+	args := strings.Split(params, ";")
+
+	switch final {
+	case 'J':
+		if len(args) == 0 || args[0] == "2" {
+			c.clearScreen()
+		}
+	case 'H':
+		row, col := 0, 0
+		if len(args) >= 1 && args[0] != "" {
+			if v, err := strconv.Atoi(args[0]); err != nil {
+				row = v - 1
+			}
+		}
+		if len(args) >= 2 && args[1] != "" {
+			if v, err := strconv.Atoi(args[1]); err == nil {
+				col = v - 1
+			}
+		}
+		c.moveCursor(row, col)
+	}
+}
+
+func (c *Console) clearScreen() {
+	for y := range c.buffer {
+		for x := range c.buffer[y] {
+			c.buffer[y][x] = ' '
+		}
+	}
+	c.cursor = image.Point{}
+}
+
+func (c *Console) moveCursor(row, col int) {
+	if row >= 0 && row < c.rows {
+		c.cursor.Y = row
+	}
+	if col >= 0 && col < c.cols {
+		c.cursor.X = col
+	}
 }
 
 func main() {

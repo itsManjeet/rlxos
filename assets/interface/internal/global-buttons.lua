9c72c13dd00e1f3f1e0d40afccddb34986af10b8
Manjeet Singh <itsmanjeet1998@gmail.com>
2025-11-28 18:30:08 +0000

    cleanup

--- assets/interface/internal/global-buttons.lua
+++
@@ -1,8 +0,0 @@
-local gears = require("gears")
-local awful = require("awful")
-
-root.buttons(gears.table.join(
-    awful.button({}, 3, function() RC.global_menu:toggle() end),
-    awful.button({}, 4, awful.tag.viewnext),
-    awful.button({}, 5, awful.tag.viewprev)
-))
